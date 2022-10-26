50731459c70d506adeb5ec8a68dd1ad865018374
Tea <74956679+teakrcmar@users.noreply.github.com>
2022-10-26 16:41:44 +0200

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,9 @@
+﻿// Hello.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Pozdrav iz aplikacije od Tee Krčmar.\n";
+}
